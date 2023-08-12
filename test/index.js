120919c1ac0e441e03d18b31aefe2fac2e097c2f
thepicture <85333076+thepicture@users.noreply.github.com>
2023-08-12 23:34:54 +0500

    feat: add png comparison

--- test/index.js
+++ test/index.js
@@ -1,14 +1,32 @@
-const assert = require('node:assert')
-const {describe} = require('node:test')
+const assert = require("node:assert");
+const { join } = require("node:path");
+const { readFileSync } = require("node:fs");
+const { describe, it } = require("node:test");
 
-const {compare} = require('../index')
+const { compare } = require("../index");
 
-describe('can call compare', () => {
+describe("png", () => {
+  const commonImagesPath = ["test", "images", "png"];
+
+  it("should return false for different png images", async () => {
     const expected = false;
-    const image1 = new ArrayBuffer(4);
-    const image2 = new ArrayBuffer(4);
+    const commonPathSegments = [...commonImagesPath, "different"];
+    const image1 = readFileSync(join(...commonPathSegments, "image1.png"));
+    const image2 = readFileSync(join(...commonPathSegments, "image2.png"));
 
-    const actual = compare(image1, image2);
+    const actual = await compare(image1, image2);
 
     assert.strictEqual(actual, expected);
-})
+  });
+
+  it("should return true for same png images", async () => {
+    const expected = true;
+    const commonPathSegments = [...commonImagesPath, "same"];
+    const image1 = readFileSync(join(...commonPathSegments, "image1.png"));
+    const image2 = readFileSync(join(...commonPathSegments, "image2.png"));
+
+    const actual = await compare(image1, image2);
+
+    assert.strictEqual(actual, expected);
+  });
+});
