35d9a3f83c8fea08daed71e737953d38926644fb
thepicture <85333076+thepicture@users.noreply.github.com>
2023-08-13 20:21:59 +0500

    feat: add jpg comparison

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const {
   getDimensions,
+  BYTE_COMPARERS,
   areBuffersEqual,
   getResizedBitmap,
-  extractBitmapChannelsWithData,
+  extractBitmapAndChannelCount,
 } = require("./lib");
 
 module.exports = {
   compare: async (buffer1, buffer2) => {
+    const magicOfBuffer1 = buffer1.at(0);
+    const magicOfBuffer2 = buffer2.at(0);
+
     const { width: width1, height: height1 } =
-      getDimensions[buffer1.at(0)](buffer1);
+      getDimensions[magicOfBuffer1](buffer1);
     const { width: width2, height: height2 } =
-      getDimensions[buffer2.at(0)](buffer2);
+      getDimensions[magicOfBuffer2](buffer2);
 
     const { channels: channels1, data: bitmap1 } =
-      await extractBitmapChannelsWithData(buffer1);
+      await extractBitmapAndChannelCount(buffer1);
     const { channels: channels2, data: bitmap2 } =
-      await extractBitmapChannelsWithData(buffer2);
+      await extractBitmapAndChannelCount(buffer2);
 
     const resizedBitmap1 = await getResizedBitmap(
       width1,
@@ -30,6 +34,11 @@ module.exports = {
       bitmap2
     );
 
-    return areBuffersEqual(resizedBitmap1, resizedBitmap2);
+    return areBuffersEqual(
+      resizedBitmap1,
+      resizedBitmap2,
+      BYTE_COMPARERS[magicOfBuffer1],
+      BYTE_COMPARERS[magicOfBuffer2]
+    );
   },
 };
