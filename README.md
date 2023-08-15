505a1938f4b61f3dbb37f52ad5bd94af7ee69b07
thepicture <85333076+thepicture@users.noreply.github.com>
2023-08-15 23:23:51 +0500

    feat: add comparator argument

--- README.md
+++ README.md
@@ -26,16 +26,94 @@ const imagePath2 = "path/to/image2.png";
 const imageBuffer1 = fs.readFileSync(imagePath1);
 const imageBuffer2 = fs.readFileSync(imagePath2);
 
-const areSame = await comparator.compare(imageBuffer1, imageBuffer2);
+const compare = async () => {
+  const areSame = await comparator.compare(imageBuffer1, imageBuffer2);
 
-if (areSame) {
-  console.log("Images are the same");
-} else {
-  console.log("Images are different");
-}
+  if (areSame) {
+    console.log("Images are the same");
+  } else {
+    console.log("Images are different");
+  }
+};
+
+compare();
 ```
 
-## Supported formats
+### With custom threshold
+
+```js
+const fs = require("node:fs");
+const comparator = require("image-comparator");
+
+const imagePath1 = "path/to/same.png";
+const imagePath2 = "path/to/same.jpg";
+
+const imageBuffer1 = fs.readFileSync(imagePath1);
+const imageBuffer2 = fs.readFileSync(imagePath2);
+
+const compare = async () => {
+  const compareFunction = (byte1, byte2) => false; // Images are always different
+
+  const areSame = await comparator.compare(
+    imageBuffer1,
+    imageBuffer2,
+    compareFunction
+  );
+
+  if (areSame) {
+    throw new Error("Should not happen");
+  } else {
+    console.log("Images are different");
+  }
+};
+
+compare();
+```
+
+```js
+const fs = require("node:fs");
+const comparator = require("image-comparator");
+
+const imagePath1 = "path/to/same.png";
+const imagePath2 = "path/to/same.jpg";
+
+const imageBuffer1 = fs.readFileSync(imagePath1);
+const imageBuffer2 = fs.readFileSync(imagePath2);
+
+const compare = async () => {
+  const compareFunction = (byte1, byte2) => Math.abs(byte1 - byte2) < 128; // If color difference is small enough
+
+  const areSame = await comparator.compare(
+    imageBuffer1,
+    imageBuffer2,
+    compareFunction
+  );
+
+  if (areSame) {
+    console.log("Images are the same");
+  } else {
+    console.log("Images are different");
+  }
+};
+
+compare();
+```
+
+# API
+
+```js
+compare: (
+  buffer1: Buffer,
+  buffer2: Buffer,
+  compareFunction: (byte1, byte2) => boolean
+) => bool;
+```
+
+Compares two images.
+
+Throws if WEBP image is compared with imag of another extension.
+
+## Supported extensions
 
 - png
 - jpg
@@ -49,5 +127,5 @@ if (areSame) {
 
 ## Notes
 
-- May produce false positives for comparison of images of different format origins due to inconsistent resulting bitmap size
+- May produce false positives for comparison of images of different format origins due to inconsistent resulting bitmap size (use `compareFunction` to avoid this)
 - Can work with interformat images, but WEBP is not supported for this at the moment
--- test/index.js
+++ test/index.js
@@ -31,6 +31,30 @@ describe("png", () => {
 
     assert.strictEqual(actual, expected);
   });
+
+  it("should perceive different images as same with low threshold", async () => {
+    const expected = true;
+    const comparator = (byte1, byte2) => Math.abs(byte1 - byte2) < 256;
+    const commonPathSegments = [...commonPngImagesPath, "different"];
+    const image1 = readFileSync(join(...commonPathSegments, "image1.png"));
+    const image2 = readFileSync(join(...commonPathSegments, "image2.png"));
+
+    const actual = await compare(image1, image2, comparator);
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it("should perceive same images as different with high threshold", async () => {
+    const expected = false;
+    const comparator = (byte1, byte2) => Math.abs(byte1 - byte2) > 255;
+    const commonPathSegments = [...commonPngImagesPath, "same"];
+    const image1 = readFileSync(join(...commonPathSegments, "image1.png"));
+    const image2 = readFileSync(join(...commonPathSegments, "image2.png"));
+
+    const actual = await compare(image1, image2, comparator);
+
+    assert.strictEqual(actual, expected);
+  });
 });
 
 describe("jpg", () => {
@@ -57,14 +81,38 @@ describe("jpg", () => {
 
     assert.strictEqual(actual, expected);
   });
+
+  it("should perceive different images as same with low threshold", async () => {
+    const expected = true;
+    const comparator = (byte1, byte2) => Math.abs(byte1 - byte2) < 256;
+    const commonPathSegments = [...commonJpgImagesPath, "different"];
+    const image1 = readFileSync(join(...commonPathSegments, "image1.jpg"));
+    const image2 = readFileSync(join(...commonPathSegments, "image2.jpg"));
+
+    const actual = await compare(image1, image2, comparator);
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it("should perceive same images as different with high threshold", async () => {
+    const expected = false;
+    const comparator = (byte1, byte2) => Math.abs(byte1 - byte2) > 255;
+    const commonPathSegments = [...commonJpgImagesPath, "same"];
+    const image1 = readFileSync(join(...commonPathSegments, "image1.jpg"));
+    const image2 = readFileSync(join(...commonPathSegments, "image2.jpg"));
+
+    const actual = await compare(image1, image2, comparator);
+
+    assert.strictEqual(actual, expected);
+  });
 });
 
 describe("webp", () => {
-  const commonJpgImagesPath = [...commonImagesPath, "webp"];
+  const commonWebpImagesPath = [...commonImagesPath, "webp"];
 
   it("should return false for different webp images", async () => {
     const expected = false;
-    const commonPathSegments = [...commonJpgImagesPath, "different"];
+    const commonPathSegments = [...commonWebpImagesPath, "different"];
     const image1 = readFileSync(join(...commonPathSegments, "image1.webp"));
     const image2 = readFileSync(join(...commonPathSegments, "image2.webp"));
 
@@ -75,11 +123,35 @@ describe("webp", () => {
 
   it("should return true for same webp images", async () => {
     const expected = true;
-    const commonPathSegments = [...commonJpgImagesPath, "same"];
+    const commonPathSegments = [...commonWebpImagesPath, "same"];
     const image1 = readFileSync(join(...commonPathSegments, "image1.webp"));
     const image2 = readFileSync(join(...commonPathSegments, "image2.webp"));
 
     const actual = await compare(image1, image2);
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it("should perceive different images as same with low threshold", async () => {
+    const expected = true;
+    const comparator = (byte1, byte2) => Math.abs(byte1 - byte2) < 256;
+    const commonPathSegments = [...commonWebpImagesPath, "different"];
+    const image1 = readFileSync(join(...commonPathSegments, "image1.webp"));
+    const image2 = readFileSync(join(...commonPathSegments, "image2.webp"));
+
+    const actual = await compare(image1, image2, comparator);
+
+    assert.strictEqual(actual, expected);
+  });
+
+  it("should perceive same images as different with high threshold", async () => {
+    const expected = false;
+    const comparator = (byte1, byte2) => Math.abs(byte1 - byte2) > 255;
+    const commonPathSegments = [...commonWebpImagesPath, "same"];
+    const image1 = readFileSync(join(...commonPathSegments, "image1.webp"));
+    const image2 = readFileSync(join(...commonPathSegments, "image2.webp"));
+
+    const actual = await compare(image1, image2, comparator);
 
     assert.strictEqual(actual, expected);
   });
@@ -129,5 +201,17 @@ describe("mixins", () => {
     const actual = () => compare(jpgImage, pngImage);
 
     assert.rejects(actual, expected);
+  });
+
+  it("should perceive same interformat images as different with high threshold", async () => {
+    const expected = false;
+    const comparator = (byte1, byte2) => Math.abs(byte1 - byte2) > 255;
+    const commonPathSegments = [...commonImagesPath, "mixins"];
+    const image1 = readFileSync(join(...commonPathSegments, "image.jpg"));
+    const image2 = readFileSync(join(...commonPathSegments, "image.png"));
+
+    const actual = await compare(image1, image2, comparator);
+
+    assert.strictEqual(actual, expected);
   });
 });
